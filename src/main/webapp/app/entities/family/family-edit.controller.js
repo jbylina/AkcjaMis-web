3eded9a4bd1fdd7b46f512b09a03763f61963534
mlagod <mateusz.lagod@gmail.com>
2016-05-18 23:51:31 +0200

    Działa kontroler. Strona edycji to kopia strony informacyjnej

--- src/main/webapp/app/entities/family/family-edit.controller.js
+++ src/main/webapp/app/entities/family/family-edit.controller.js
@@ -8,21 +8,20 @@
         .module('akcjamisApp')
         .controller('FamilyEditController', FamilyEditController);
 
-    FamilyEditController.$inject = ['$stateParams', 'entity', 'Family', 'FamilyNote'];
+    FamilyEditController.$inject = ['$stateParams', 'entity', 'Family', '$resource'];
 
     function FamilyEditController($stateParams, entity, Family, $resource){
 
         var vm = this;
         vm.family = entity;
-        vm.familyNotes = $resource('/api/families/:id/family-notes', {id:$stateParams.id}).query();
+        vm.familyNotes = $resource('api/families/:id/family-notes', {id:$stateParams.id}).query();
         vm.childrens = $resource('/api/families/:id/children', {id:$stateParams.id}).query();
         vm.contacts = $resource('/api/families/:id/contacts', {id:$stateParams.id}).query();
-
 
         vm.load = function(id){
             Family.get({id: id}, function(result) {
                 vm.family = result;
             });
-        }
+        };
     }
-});
+})();
